b189766691d979ea038ab7af2ee93de2e1f5aadf
analogMensch <105582811+analogMensch@users.noreply.github.com>
2022-11-23 18:46:26 +0100

    Add files via upload

---
+++ WirelessData.h
@@ -0,0 +1,10 @@
+// WirelessData.h
+
+#ifndef _WIRELESSDATA_H
+#define _WIRELESSDATA_H
+
+const char* ssid = "WiFiSSID";  //SSID
+const char* password = "WiFiPASSWORD";  //password
+const char* ntpServer = "0.0.0.0";  //time server (router IP or pool.ntp.org)
+    
+#endif  // _WIRELESSDATA_H
